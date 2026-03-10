deac42dee71b3ba39a7dbb04ee427f4989095287
Afthar17 <aftharafthar5@gmail.com>
2026-03-10 19:33:19 +0530

    cookie error

--- server/src/utils/genToken.js
+++ server/src/utils/genToken.js
@@ -8,7 +8,7 @@ export const genToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
