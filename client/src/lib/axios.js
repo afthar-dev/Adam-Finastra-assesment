ad7874edd0e05ce32dfccfd36ba78a0bdec7904c
Afthar17 <aftharafthar5@gmail.com>
2026-03-10 19:23:30 +0530

    vercel issue fixed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.PROD
-    ? "https://adam-finastra-assesment.onrender.com/api"
-    : "http://localhost:5000/api",
+  baseURL: "https://adam-finastra-assesment.onrender.com/api",
   withCredentials: true,
 });
 
